7a5af4826b349e444e820675b6effaf75ea47834
XeniaPhe <m.tunahanbas90@gmail.com>
2022-05-31 17:49:43 +0300

    wave motion upgraded

--- Assets/Scripts/Game Components/Peg.cs
+++ Assets/Scripts/Game Components/Peg.cs
@@ -4,33 +4,97 @@ namespace BattleShips.GameComponents
 {
     internal class Peg : MonoBehaviour
     {
-        [SerializeField] Vector3 dragSpeed;
-        [SerializeField] Vector3 dragAmount;
-        [SerializeField] Vector3 rotationSpeed;
-        [SerializeField] Vector3 rotationAmount;
+        #region Serialized Fields
+
+        [Header("Continuous Motion Settings")]
+        [SerializeField] float dragSpeedMin;
+        [SerializeField] float dragSpeedMax;
+        [SerializeField] float dragAmountMin;
+        [SerializeField] float dragAmountMax;
+        [SerializeField] float rotationSpeedMin;
+        [SerializeField] float rotationSpeedMax;
+        [SerializeField] float rotationAmountMin;
+        [SerializeField] float rotationAmountMax;
         [SerializeField] float randomTranslation;
         [SerializeField] float randomRotation;
+
+        [Header("Shake Settings")]
+        [SerializeField] float shakeDragSpeedMin;
+        [SerializeField] float shakeDragSpeedMax;
+        [SerializeField] float shakeDragMin;
+        [SerializeField] float shakeDragMax;
+        [SerializeField] float shakeRotationSpeedMin;
+        [SerializeField] float shakeRotationSpeedMax;
+        [SerializeField] float shakeRotationMin;
+        [SerializeField] float shakeRotationMax;
+        [SerializeField] float shakeDuration;
+
+        #endregion
+
+        #region Cached Fields
         float t;
+        float shakeBeginningTime;
+        bool isShaking;
+
+        Vector3 dragSpeed;
+        Vector3 dragAmount;
+        Vector3 rotationSpeed;
+        Vector3 rotationAmount;
+        Vector3 shakeDragSpeed;
+        Vector3 shakeDragAmount;
+        Vector3 shakeRotationSpeed;
+        Vector3 shakeRotationAmount;
+
+
+        #endregion
 
         private void Awake()
         {
             randomRotation = Mathf.Abs(randomRotation);
             randomTranslation = Mathf.Abs(randomTranslation);
+            InitializeRandom();
         }
+
+        internal void InitializeRandom()
+        {
+            dragSpeed = new Vector3(Random.Range(dragSpeedMin, dragSpeedMax), Random.Range(dragSpeedMin, dragSpeedMax), Random.Range(dragSpeedMin, dragSpeedMax));
+            dragAmount = new Vector3(Random.Range(dragAmountMin, dragAmountMax), Random.Range(dragAmountMin, dragAmountMax), Random.Range(dragAmountMin, dragAmountMax));
+            rotationSpeed = new Vector3(Random.Range(rotationSpeedMin, rotationSpeedMax), Random.Range(rotationSpeedMin, rotationSpeedMax), Random.Range(rotationSpeedMin, rotationSpeedMax));
+            rotationAmount = new Vector3(Random.Range(rotationAmountMin, rotationAmountMax), Random.Range(rotationAmountMin, rotationAmountMax), Random.Range(rotationAmountMin, rotationAmountMax));
+        }
+
         private void Update()
         {
             t = Time.time;
-            transform.position = new Vector3(CalculateMotion(t, dragSpeed.x, dragAmount.x),
-                CalculateMotion(t, dragSpeed.y, dragAmount.y), CalculateMotion(t, dragSpeed.z, dragAmount.z)) + GetRandom(randomTranslation);
-            transform.rotation = Quaternion.Euler(new Vector3( CalculateMotion(t,rotationSpeed.x,rotationAmount.x),
-                CalculateMotion(t,rotationSpeed.y,rotationAmount.y),CalculateMotion(t,rotationSpeed.z,rotationAmount.z)) + GetRandom(randomRotation));
+
+            if(isShaking)
+            {
+
+
+                if(t - shakeBeginningTime > shakeDuration)
+                    isShaking = false;
+            }
+            else
+            {
+                transform.position = new Vector3(CalculateMotion(dragSpeed.x, dragAmount.x),
+                    CalculateMotion(dragSpeed.y, dragAmount.y), CalculateMotion(dragSpeed.z, dragAmount.z)) + GetRandom(randomTranslation);
+                transform.rotation = Quaternion.Euler(new Vector3(CalculateMotion(rotationSpeed.x, rotationAmount.x),
+                    CalculateMotion(rotationSpeed.y, rotationAmount.y), CalculateMotion(rotationSpeed.z, rotationAmount.z)) + GetRandom(randomRotation));
+            }
+        }
+        internal void Shake()
+        {
+            shakeBeginningTime = t;
+            isShaking = true;
         }
 
-        float CalculateMotion(float time,float speed,float amount) => Mathf.Sin(time * speed) * amount;
+        float CalculateMotion(float speed,float amount) => Mathf.Sin(t * speed) * amount;
 
+        //We can always remove that if fps drops immensely as more pegs get placed
         Vector3 GetRandom(float distance)
         {
             return new Vector3(Random.Range(-distance, distance), Random.Range(-distance, distance), Random.Range(-distance, distance));
         }
+        
     }
 }
